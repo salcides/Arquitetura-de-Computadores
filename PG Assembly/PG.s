49c7ea207d3dc19e5ea985cfa122399edd83f7f5
salcides <38273343+salcides@users.noreply.github.com>
2018-06-22 00:32:07 -0300

    Update PG.s

--- PG Assembly/PG.s
+++ PG Assembly/PG.s
@@ -1,16 +1,16 @@
 ;Trabalho de Arquitetura de Computadores 2018-01
-;IFES - Engenharia de Controle e AutomaÁ„o
+;IFES - Engenharia de Controle e Automa√ß√£o
 ;Igor Rodrigues Salcides e Gabriel Saade Pagani
-;PG de raz„o 2
+;PG de raz√£o 2
 
 
-;Verificar depois com o professor a inicializaÁ„o de vari·veis
-		MOV R1, #1	;R1 ser· o contador
-;X = 40 (posiÁ„o base da dupla)
+;Verificar depois com o professor a inicializa√ß√£o de vari√°veis
+		MOV R1, #1	;R1 ser√° o contador
+;X = 4 (posi√ß√£o base*100 e registrador RX da dupla)
 ;
 ;
 
-WHILE		CMP R1, #10 	;Verificando condiÁ„o de parada
+WHILE		CMP R1, #10 	;Verificando condi√ß√£o de parada
 		BEQ GG		;10 termos: vai pro final
 		LSL R, #2	;Multiplica o valor base por 2
 
